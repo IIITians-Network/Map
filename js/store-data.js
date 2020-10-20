163fdeb894d60008feec123ec2d0514a07406233
vimalverma558 <kumarvimal558@gmail.com>
2020-10-20 15:08:06 +0530

    added all iiit information

--- js/store-data.js
+++ js/store-data.js
@@ -1,4 +1,28 @@
-let stores = [{
+let stores = [
+{
+    "name": "IIIT Agartala",
+    "phoneNumber": "0381 254 6629",
+    "coordinates": {
+        "latitude": 23.8408767,
+        "longitude": 91.4214196
+    },
+    "city": "Agartala",
+    "address": {
+        "streetAddressLine1": "IIIT Agartala",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Agartala",
+        "countrySubdivisionCode": "Tripura",
+        "countryCode": "IN",
+        "postalCode": "799046"
+    },
+    "website" : "https://www.nita.ac.in/IIITAgartala/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Agartala", "IBarjala, Jirania, Agartala, Tripura 799046"]
+},
+{
     "name": "IIIT Allahabad",
     "phoneNumber": "310-659-9562",
     "coordinates": {
@@ -20,7 +44,514 @@ let stores = [{
     "open": true,
     "openStatusText": "Open until 6:00 PM",
     "addressLines": ["IIIT Allahabad", "Jhalwa, Prayagraj, Uttar Pradesh 211015"]
-}, {
+},
+{
+    "name": "IIIT Bhagalpur",
+    "phoneNumber": " 076329 95210",
+    "coordinates": {
+        "latitude": 25.2543109,
+        "longitude": 87.0444615
+    },
+    "city": "Bhagalpur",
+    "address": {
+        "streetAddressLine1": "IIIT Bhagalpur",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Bhagalpur",
+        "countrySubdivisionCode": "Bihar",
+        "countryCode": "IN",
+        "postalCode": "813210"
+    },
+    "website" : "https://www.iiitbh.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Bhagalpur", "College of Engineering Campus, Bhagalpur, Sabour, Bihar 813210"]
+},
+{
+    "name": "IIIT Bhopal",
+    "phoneNumber": "0755 405 1950",
+    "coordinates": {
+        "latitude": 23.217693,
+        "longitude": 77.4086312
+    },
+    "city": "Bhopal",
+    "address": {
+        "streetAddressLine1": "IIIT Bhopal",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Bhopal",
+        "countrySubdivisionCode": "MP",
+        "countryCode": "IN",
+        "postalCode": "462003"
+    },
+    "website" : "https://www.iiitbhopal.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Bhopal", "Maulana Azad National Institute of Technology, Madhya Pradesh 462003"]
+},
+{
+    "name": "IIIT Dharward",
+    "phoneNumber": "0836 2250879",
+    "coordinates": {
+        "latitude": 15.3535312,
+        "longitude": 75.1319445
+    },
+    "city": "Dharward",
+    "address": {
+        "streetAddressLine1": "IIIT Dharward",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Dharward",
+        "countrySubdivisionCode": "Karnataka",
+        "countryCode": "IN",
+        "postalCode": "580029"
+    },
+    "website" : "https://www.iiitdwd.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Dharward", "3rd floor, IT park, Hubli - Dharwad Hwy, Deshpande Nagar, Karnataka 580029"]
+},
+{
+    "name": "IIIT Guwahati",
+    "phoneNumber": "0361 263 0015",
+    "coordinates": {
+        "latitude": 26.0849317,
+        "longitude": 91.5348684
+    },
+    "city": "Guwahati",
+    "address": {
+        "streetAddressLine1": "IIIT Guwahati",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Guwahati",
+        "countrySubdivisionCode": "Assam",
+        "countryCode": "IN",
+        "postalCode": "781015"
+    },
+    "website" : "https://www.iiitg.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Guwahati", "Gauhati, NH37, Bongara, Assam 781015"]
+},
+{
+    "name": "IIITM Gwalior",
+    "phoneNumber": "+917512449801",
+    "coordinates": {
+        "latitude": 26.2494521,
+        "longitude": 78.1741388
+    },
+    "city": "Gwalior",
+    "address": {
+        "streetAddressLine1": "IIITM Gwalior",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Gwalior",
+        "countrySubdivisionCode": "MP",
+        "countryCode": "IN",
+        "postalCode": "474015"
+    },
+    "website" : "https://www.iiitm.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIITM Gwalior", "Morena Link Rd, IIITM Campus, Gwalior, Madhya Pradesh 474015"]
+},
+{
+    "name": "IIITDM Jabalpur",
+    "phoneNumber": "076127 94094",
+    "coordinates": {
+        "latitude": 23.1769205,
+        "longitude": 80.0245881
+    },
+    "city": "Jabalpur",
+    "address": {
+        "streetAddressLine1": "IIITDM Jabalpur",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Jabalpur",
+        "countrySubdivisionCode": "Mp",
+        "countryCode": "IN",
+        "postalCode": "482005"
+    },
+    "website" : "https://www.iiitdmj.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIITDM Jabalpur", "Airport Rd, PDPM IIITDM Jabalpur Campus, Khamaria, Jabalpur, Madhya Pradesh 482005"]
+},
+{
+    "name": "IIIT Kalyani",
+    "phoneNumber": "098360 75844",
+    "coordinates": {
+        "latitude": 22.9610624,
+        "longitude": 88.4334444
+    },
+    "city": "Kalyani",
+    "address": {
+        "streetAddressLine1": "IIIT Kalyani",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Kalyani",
+        "countrySubdivisionCode": "West Bengal",
+        "countryCode": "IN",
+        "postalCode": "741235"
+    },
+    "website" : "https://www.iiitKalyani.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Kalyani", "near Webel IT-Park, Phase 3, Block A, Kalyani, West Bengal 741235"]
+},
+{
+    "name": "IIITDM Kancheepuram",
+    "phoneNumber": "310-659-9562",
+    "coordinates": {
+        "latitude": 12.8394963,
+        "longitude": 80.1374898
+    },
+    "city": "Kancheepuram",
+    "address": {
+        "streetAddressLine1": "IIITDM Kancheepuram",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Kancheepuram",
+        "countrySubdivisionCode": "Tamil Nadu",
+        "countryCode": "IN",
+        "postalCode": "600127"
+    },
+    "website" : "https://www.iiitdm.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIITDM Kancheepuram", "Melakottaiyur Village,Near Kandigai, Off Vandalur-Kelambakkam Road, Nellikuppam, Chennai, Tamil Nadu 600127"]
+},
+{
+    "name": "IIIT Kota",
+    "phoneNumber": "+911412715071",
+    "coordinates": {
+        "latitude": 26.8640443,
+        "longitude": 75.810796
+    },
+    "city": "Kota",
+    "address": {
+        "streetAddressLine1": "IIIT Kota",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Kota",
+        "countrySubdivisionCode": "Rajasthan",
+        "countryCode": "IN",
+        "postalCode": "302017"
+    },
+    "website" : "https://www.iiitkota.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Kota", "2nd Floor, Prabha Bhawan, MNIT Campus,, JLN Marg, Jaipur, Rajasthan 302017"]
+},
+{
+    "name": "IIIT Kottayam",
+    "phoneNumber": "+914712597459",
+    "coordinates": {
+        "latitude": 9.7551464,
+        "longitude": 76.6500117
+    },
+    "city": "Kottayam",
+    "address": {
+        "streetAddressLine1": "IIIT Kottayam",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Kottayam",
+        "countrySubdivisionCode": "Kerala",
+        "countryCode": "IN",
+        "postalCode": "686635"
+    },
+    "website" : "https://www.iiitKottayam.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Kottayam", "Valavoor - Chakkampuzha Rd, Valavoor, Kerala 686635"]
+},
+{
+    "name": "IIITDM Kurnool",
+    "phoneNumber": "085182 89114",
+    "coordinates": {
+        "latitude": 15.7617739,
+        "longitude": 78.036388
+    },
+    "city": "Kurnool",
+    "address": {
+        "streetAddressLine1": "IIIT Kurnool",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Kurnool",
+        "countrySubdivisionCode": "Andhra Pradesh",
+        "countryCode": "IN",
+        "postalCode": "518007"
+    },
+    "website" : "http://www.iiitdmkl.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Kurnool", "Jagannatha gattu, Dinnedevarapadu, Andhra Pradesh 518007"]
+},
+{
+    "name": "IIIT Lucknow",
+    "phoneNumber": "0",
+    "coordinates": {
+        "latitude": 26.8008689,
+        "longitude": 81.02403
+    },
+    "city": "Lucknow",
+    "address": {
+        "streetAddressLine1": "IIIT Lucknow",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Lucknow",
+        "countrySubdivisionCode": "UP",
+        "countryCode": "IN",
+        "postalCode": "226002"
+    },
+    "website" : "https://www.iiitl.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Lucknow", "Unnamed Road, Ahmamau, Uttar Pradesh 226002"]
+},
+{
+    "name": "IIIT Manipur",
+    "phoneNumber": "+913852421017",
+    "coordinates": {
+        "latitude": 24.8429901,
+        "longitude": 93.9384447
+    },
+    "city": "Heingang",
+    "address": {
+        "streetAddressLine1": "IIIT Manipur",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Heingang",
+        "countrySubdivisionCode": "Manipur",
+        "countryCode": "IN",
+        "postalCode": "795002"
+    },
+    "website" : "https://www.iiitManipur.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Manipur", "National Highway 39, Mantripukhri, Heingang, Manipur 795002"]
+},
+{
+    "name": "IIIT Nagpur",
+    "phoneNumber": "0",
+    "coordinates": {
+        "latitude": 21.1646529,
+        "longitude": 79.0547401
+    },
+    "city": "Nagpur",
+    "address": {
+        "streetAddressLine1": "IIIT Nagpur",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Nagpur",
+        "countrySubdivisionCode": "Maharashtra",
+        "countryCode": "IN",
+        "postalCode": "440006"
+    },
+    "website" : "https://www.iiitn.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Nagpur", "Bsnl rrtc center, Near TV Tower, Seminary Hills, Nagpur, Maharashtra 440006"]
+},
+{
+    "name": "IIIT Pune",
+    "phoneNumber": "0",
+    "coordinates": {
+        "latitude": 18.4206487,
+        "longitude": 73.9048304
+    },
+    "city": "Pune",
+    "address": {
+        "streetAddressLine1": "IIIT Pune",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Pune",
+        "countrySubdivisionCode": "Maharashtra",
+        "countryCode": "IN",
+        "postalCode": "411048"
+    },
+    "website" : "https://www.iiitp.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Pune", "Near Bopdev Ghat, Kondhwa Annexe, Yewalewadi, Pune, Maharashtra 411048"]
+},
+{
+    "name": "IIIT Raichur",
+    "phoneNumber": "+914023016033",
+    "coordinates": {
+        "latitude": 17.5947078,
+        "longitude": 78.1208514
+    },
+    "city": "Raichur",
+    "address": {
+        "streetAddressLine1": "IIIT Raichur",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Raichur",
+        "countrySubdivisionCode": "Telangana",
+        "countryCode": "IN",
+        "postalCode": "502285"
+    },
+    "website" : "https://www.iiitr.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Raichur", "IITH Main Road, Near NH-65, Sangareddy, Kandi, Telangana 502285"]
+},
+{
+    "name": "IIIT Ranchi",
+    "phoneNumber": "0",
+    "coordinates": {
+        "latitude": 23.3155863,
+        "longitude": 85.3745218
+    },
+    "city": "Ranchi",
+    "address": {
+        "streetAddressLine1": "IIIT Ranchi",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Ranchi",
+        "countrySubdivisionCode": "Jharkhand",
+        "countryCode": "IN",
+        "postalCode": "834010"
+    },
+    "website" : "https://www.iiitRanchi.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Ranchi", "Sirkha Toli, Kali Nagar, Namkum, Ranchi, Jharkhand 834010"]
+},
+{
+    "name": "IIIT Sonepat",
+    "phoneNumber": "+917496966902",
+    "coordinates": {
+        "latitude": 28.9519722,
+        "longitude": 77.1039977
+    },
+    "city": "Sonepat",
+    "address": {
+        "streetAddressLine1": "IIIT Sonipat",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Sonipat",
+        "countrySubdivisionCode": "Haryana",
+        "countryCode": "IN",
+        "postalCode": "0"
+    },
+    "website" : "https://www.iiitSonipat.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Sonipat", "IIT Delhi Sonipat Campus, Sonipat,Haryana"]
+},
+{
+    "name": "IIIT Sri City",
+    "phoneNumber": "0",
+    "coordinates": {
+        "latitude": 13.5602786,
+        "longitude": 80.0268402
+    },
+    "city": "Sri City",
+    "address": {
+        "streetAddressLine1": "IIIT Sri City",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Sri City",
+        "countrySubdivisionCode": "AP",
+        "countryCode": "IN",
+        "postalCode": "517646"
+    },
+    "website" : "https://www.iiits.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Sri City", "Mandal 517 646, 630, Gnan Marg, Sri City, Sathyavedu, Andhra Pradesh 517646"]
+},
+{
+    "name": "IIIT Surat",
+    "phoneNumber": "0",
+    "coordinates": {
+        "latitude": 21.1643024,
+        "longitude": 72.7835424
+    },
+    "city": "Surat",
+    "address": {
+        "streetAddressLine1": "IIIT Surat",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Surat",
+        "countrySubdivisionCode": "Gujarat",
+        "countryCode": "IN",
+        "postalCode": "395007"
+    },
+    "website" : "https://www.iiitsurat.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Surat", "SVNIT Campus, Athwa, Surat, Gujarat 395007"]
+},
+{
+    "name": "IIIT Tiruchirapalli",
+    "phoneNumber": "+919489600124",
+    "coordinates": {
+        "latitude": 10.7545443,
+        "longitude": 78.8218047
+    },
+    "city": "Tiruchirapalli",
+    "address": {
+        "streetAddressLine1": "IIIT Tiruchirapalli",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Tiruchirapalli",
+        "countrySubdivisionCode": "Tamil Nadu",
+        "countryCode": "IN",
+        "postalCode": "620015"
+    },
+    "website" : "https://www.iiitt.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Tiruchirapalli", "NIT Campus, Tanjore Main Road, near BHEL, Tiruchirappali, Tamil Nadu 620015"]
+},
+{
+    "name": "IIIT Una",
+    "phoneNumber": "0",
+    "coordinates": {
+        "latitude": 31.4808162,
+        "longitude": 76.1991189
+    },
+    "city": "Una",
+    "address": {
+        "streetAddressLine1": "IIIT Una",
+        "streetAddressLine2": null,
+        "streetAddressLine3": null,
+        "city": "Allahabad",
+        "countrySubdivisionCode": "HP",
+        "countryCode": "IN",
+        "postalCode": "177209"
+    },
+    "website" : "https://www.iiitu.ac.in/",
+    "ownershipTypeCode": "GOI",
+    "open": true,
+    "openStatusText": "Open until 6:00 PM",
+    "addressLines": ["IIIT Allahabad", "Saloh, Una, Himachal Pradesh 177209"]
+},
+{
     "name": "IIIT Vadodara (Gandhinagar Campus)",
     "phoneNumber": "(323) 634-7845",
     "coordinates": {
@@ -42,7 +573,8 @@ let stores = [{
     "open": true,
     "openStatusText": "Open until 6:00 PM",
     "addressLines": ["IIIT Vadodara (Gandhinagar Campus)", " Sector 28, Gandhinagar, Gujarat 382028"]
-}, {
+},
+{
     "name": "IIIT Vadodara - Diu Campus",
     "phoneNumber": "323-602-0637",
     "coordinates": {
